e9edec9953e9e2a34d067790b2a7e804c6f7e515
MoonNight285 <kimjyjh11@naver.com>
2022-11-02 15:59:21 +0900

    chap13 Gson 예제

---
+++ src/main/java/com/bitc/jspchap13/Student.java
@@ -0,0 +1,27 @@
+package com.bitc.jspchap13;
+
+public class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
